23e058dab7e1ba8f574ef196a06202ca2a51368d
Smohana-Priya <smohanapriya2604@gmail.co>
2024-04-08 12:08:31 +0530

    code cleanup

--- lib/home.dart
+++ lib/home.dart
@@ -43,37 +43,33 @@ class _HomeState extends State<Home> {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Center(child: Text('Firebase Upload')),
+          title: const Center(child: Text('File Uploading - Firebase')),
         ),
         body: ListView.builder(
           itemCount: fileUrls.length,
           itemBuilder: (context, index) {
             return Padding(
-              padding: const EdgeInsets.only(left: 10,right: 10,top: 10),
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
               child: Container(
                 decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondaryContainer,
-                    borderRadius: BorderRadius.circular(10)
-                ),
-
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    borderRadius: BorderRadius.circular(10)),
                 child: ListTile(
-                  leading: CircleAvatar(child: Image.network(fileUrls[index])), // Show image from URL
-                  title: Text(pickedFile!.name,style:const TextStyle(fontSize: 13),), // Show filename
+                  leading: CircleAvatar(child: Image.network(fileUrls[index])),
+                  title: Text(
+                    pickedFile!.name,
+                    style: const TextStyle(fontSize: 13),
+                  ),
                 ),
               ),
             );
           },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await selectFile();
-          },
+          onPressed: selectFile,
           child: const Icon(Icons.upload),
         ),
       ),
     );
   }
-
-
-
 }
